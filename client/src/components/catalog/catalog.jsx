2a639c6bee52ff035a6d53aedba1eb8cff290260
NaN-niki <niki_stst@abv.bg>
2023-11-28 22:49:01 +0200

    Show dynamic Game list - Catalog

--- client/src/components/catalog/catalog.jsx
+++ client/src/components/catalog/catalog.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import * as userService from '../../services/gameService'
+import { GameItem } from "./gameItem.jsx/gameItem"
 
 export function Catalog() {
     const [games, setGames] = useState([])
@@ -11,19 +12,11 @@ export function Catalog() {
     }, [])
 
     return (
-        // <section id="catalog-page">
-        //     <h1>All Games</h1>
-        //     <div className="allGames">
-        //         <div className="allGames-info">
-        //             <img src="./images/avatar-1.jpg" />
-        //             <h6>Action</h6>
-        //             <h2>Cover Fire</h2>
-        //             <a href="#" className="details-button">Details</a>
-        //         </div>
-
-        //     </div>
-
-        //     <h3 className="no-articles">No articles yet</h3>
-        // </section>
+        <section id="catalog-page">
+            <h1>All Games</h1>
+            {games.map(game => <GameItem key={game._id} data={game}/>)}
+           
+           {games.length === 0 &&  <h3 className="no-articles">No articles yet</h3>}
+        </section>
     )
 }
---
+++ client/src/components/catalog/gameItem.jsx/gameItem.jsx
@@ -0,0 +1,12 @@
+export function GameItem({data}) {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={data.imageUrl} />
+                <h6>{data.category}</h6>
+                <h2>{data.title}</h2>
+                <a href="#" className="details-button">Details</a>
+            </div>
+        </div>
+    )
+}
