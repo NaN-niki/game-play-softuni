fd495a7648295f0cf5c93235776f2ce23d79205a
NaN-niki <niki_stst@abv.bg>
2023-11-28 23:22:26 +0200

    Making requests as partial application

--- client/src/lib/reuest.js
+++ client/src/lib/reuest.js
@@ -1,20 +1,19 @@
-export const request = async (method, url, body) => {
-
+const request = async (method, url, body) => {
     let options = {}
 
-    if(body){
+    if (body) {
         options = {
             method,
-            headers : {
-                'content-type' : 'application/json',
+            headers: {
+                'content-type': 'application/json',
             },
-            body : JSON.stringify(body)
+            body: JSON.stringify(body)
         }
     }
 
     try {
         const response = await fetch(url, options)
-        
+
         if (!response.ok) {
             throw new Error('')
         }
@@ -24,3 +23,11 @@ export const request = async (method, url, body) => {
         console.log(error.msg)
     }
 }
+
+//partial application
+// ne ni interesuva konteksta za tova toj e null no zadavame properties
+//vtoriq parametur go podavame kato purvi argument na zaqvkite 
+export const get = request.bind(null, 'GET')
+export const post = request.bind(null, 'POST')
+export const put = request.bind(null, 'PUT')
+export const del = request.bind(null, 'DELETE')
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,12 +1,12 @@
-import { request } from "../lib/reuest"
+import * as request from "../lib/reuest"
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
-export const create = async (data) => {
-    return await request('POST', baseUrl, data)
+export const getAll = async () => {
+    const result = await request.get(baseUrl)
+    return Object.values(result)
 }
 
-export const getAll = async () => {
-    const result = await request('GET', baseUrl)
-    return Object.values(result)
+export const create = async (data) => {
+    return await request.post(baseUrl, data)
 }
